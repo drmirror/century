ae0cc223cc81f35e464cd49fd7330b70586416a7
A. Jesse Jiryu Davis <jesse@mongodb.com>
2014-04-19 15:38:10 -0400

    Matplotlib triangulation with refined isometric coloring.

--- web/experiments/triplot_demo.py
+++ web/experiments/triplot_demo.py
@@ -2,24 +2,39 @@
 Creating and plotting unstructured triangular grids.
 """
 import matplotlib.pyplot as plt
+from matplotlib.tri import UniformTriRefiner
+from matplotlib.tri.triangulation import Triangulation
+import matplotlib.cm as cm
 import numpy as np
 
-xy = np.asarray([
-    [-0.101,0.872],[-0.080,0.883],[-0.069,0.888],[-0.054,0.890],[-0.045,0.897],
-    [-0.057,0.895],[-0.073,0.900],[-0.087,0.898],[-0.090,0.904],[-0.069,0.907],
+xytemp = np.asarray([
+    [-0.101,0.872,17],[-0.080,0.883,11],[-0.069,0.888,22],[-0.054,0.890,27],
+    [-0.057,0.895,9],[-0.073,0.900,14],[-0.087,0.898,17],[-0.090,0.904,16],
 ])
-x = xy[:,0]*180/3.14159
-y = xy[:,1]*180/3.14159
 
-# Rather than create a Triangulation object, can simply pass x, y and triangles
-# arrays to triplot directly.  It would be better to use a Triangulation object
-# if the same triangulation was to be used more than once to save duplicated
-# calculations.
+
+x = xytemp[:,0]
+y = xytemp[:,1]
+z = xytemp[:,2]
+
+triangulation = Triangulation(x, y)
+#-----------------------------------------------------------------------------
+# Refine data
+#-----------------------------------------------------------------------------
+refiner = UniformTriRefiner(triangulation)
+tri_refi, z_test_refi = refiner.refine_field(z, subdiv=3)
+
 plt.figure()
 plt.gca().set_aspect('equal')
-plt.triplot(x, y, 'ko-')
-plt.title('auto triplot')
-plt.xlabel('Longitude (degrees)')
-plt.ylabel('Latitude (degrees)')
+plt.triplot(triangulation, lw=0.5, color='white')
+
+levels = np.arange(0., 30., 1)
+cmap = cm.get_cmap(name='terrain', lut=None)
+plt.tricontourf(tri_refi, z_test_refi, levels=levels, cmap=cmap)
+plt.tricontour(tri_refi, z_test_refi, levels=levels,
+               colors=['0.25', '0.5', '0.5', '0.5', '0.5'],
+               linewidths=[1.0, 0.5, 0.5, 0.5, 0.5])
+
+plt.title("High-resolution tricontouring")
 
 plt.show()
