2db00c3b36a68a1fafc05969219b02d7a10941e1
A. Jesse Jiryu Davis <jesse@mongodb.com>
2014-04-19 16:02:39 -0400

    Use real weather data for image.

--- web/experiments/triplot_demo.py
+++ web/experiments/triplot_demo.py
@@ -1,21 +1,60 @@
 """
 Creating and plotting unstructured triangular grids.
 """
+import datetime
+import time
+
 import matplotlib.pyplot as plt
 from matplotlib.tri import UniformTriRefiner
 from matplotlib.tri.triangulation import Triangulation
 import matplotlib.cm as cm
 import numpy as np
+import pymongo
 
-xytemp = np.asarray([
-    [-0.101,0.872,17],[-0.080,0.883,11],[-0.069,0.888,22],[-0.054,0.890,27],
-    [-0.057,0.895,9],[-0.073,0.900,14],[-0.087,0.898,17],[-0.090,0.904,16],
-])
+db = pymongo.MongoClient().ncdc
 
+# Positions of stations active in this hour. Needs index on 'ts'.
+dt = datetime.datetime(1978, 10, 01, 10)
+next_hour = dt + datetime.timedelta(hours=1)
+
+pipeline = [{
+    '$match': {
+        'ts': {'$gte': dt, '$lt': next_hour},
+        # Valid samples.
+        'airTemperature.quality': '1'
+    }
+}, {
+    '$group': {
+        '_id': '$st',
+        'position': {'$first': '$position'},
+        # Could average the temperatures, probably not worthwhile.
+        'airTemperature': {'$first': '$airTemperature'},
+    }
+}]
+
+start = time.time()
+cursor = db.data.aggregate(pipeline=pipeline, cursor={})
+n = 0
+data = []
+for doc in cursor:
+    n += 1
+    try:
+        lng, lat = doc['position']['coordinates']
+        temp = doc['airTemperature']['value']
+    except (TypeError, KeyError):
+        # Incomplete.
+        pass
+    else:
+        data.append([lng, lat, temp])
+
+
+xytemp = np.asarray(data)
 
 x = xytemp[:,0]
 y = xytemp[:,1]
 z = xytemp[:,2]
+
+
 
 triangulation = Triangulation(x, y)
 #-----------------------------------------------------------------------------
@@ -28,7 +67,10 @@ plt.figure()
 plt.gca().set_aspect('equal')
 plt.triplot(triangulation, lw=0.5, color='white')
 
-levels = np.arange(0., 30., 1)
+# Number of levels among temperatures.
+zmin = int(z.min()) - 1
+zmax = int(z.max()) + 1
+levels = np.arange(zmin, zmax, (zmax - zmin) / 20.)
 cmap = cm.get_cmap(name='terrain', lut=None)
 plt.tricontourf(tri_refi, z_test_refi, levels=levels, cmap=cmap)
 plt.tricontour(tri_refi, z_test_refi, levels=levels,
