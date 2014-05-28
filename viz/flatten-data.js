68c5189739f815d87698508b607c73b4dc29afbc
A. Jesse Jiryu Davis <jesse@mongodb.com>
2014-05-28 15:53:53 -0400

    A script to flatten the ncdc.data collection into something Monary can query.

---
+++ viz/flatten-data.js
@@ -0,0 +1,27 @@
+/*
+ * Convert ncdc.data collection into something flat that Monary can query.
+ */
+
+db.data.aggregate([{
+    $match: {
+        ts: {$gt: ISODate("2013-12-00T00:00:00")},
+        // Valid temperature samples.
+        'airTemperature.quality': '1',
+        // Positions of 0, 0 are probably invalid.
+        'position.coordinates': {'$ne': [0, 0]}
+    }
+}, {
+    $unwind: "$position.coordinates"
+}, {
+    $group: {
+        _id: '$_id',
+        t: {$first: '$ts'},
+        x: {$first: '$position.coordinates'},
+        y: {$last: '$position.coordinates'},
+        a: {$first: '$airTemperature.value'}
+    }
+}, {
+    $out: 'flattened'
+}], {allowDiskUse: true});
+
+db.flattened.ensureIndex({ts: 1});
