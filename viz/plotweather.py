b469a3ad4e49b619f8ad17b5b29523d4f5fa38f4
A. Jesse Jiryu Davis <jesse@mongodb.com>
2014-05-28 13:26:34 -0400

    Refactor plotweather.

--- viz/plotweather.py
+++ viz/plotweather.py
@@ -99,20 +99,7 @@ def stations(options, dt):
     return ret
 
 
-options, start, end = parse_options(sys.argv)
-
-station_data = np.array(stations(options, start))
-longitudes = station_data[:, 0]
-latitudes = station_data[:, 1]
-temperatures = station_data[:, 2]
-
-# make orthographic basemap.
-m = Basemap(resolution='c', projection='cyl', lat_0=60., lon_0=-60.)
-
-x, y = m(longitudes, latitudes)
-
-
-def expand_earth(x, y):
+def expand_earth(x, y, temperatures):
     """Surround Earth with copies to simulate spherical wrapping.
 
     Add earths to the left and right of earth, and vertically-flipped copies
@@ -158,35 +145,53 @@ def expand_earth(x, y):
     print 'tiling took %.2f sec' % (time.time() - start)
     return x_expanded, y_expanded, temperatures_expanded
 
-x_expanded, y_expanded, temperatures_expanded = expand_earth(x, y)
 
-# set desired contour levels.
-clevs = np.arange(temperatures.min() - 10, temperatures.max() + 10, 5)
+def main(argv):
+    options, start, end = parse_options(argv)
 
-# create figure, add axes
-fig1 = plt.figure(figsize=(8, 10))
-ax = fig1.add_axes([0.1, 0.1, 0.8, 0.8])
+    station_data = np.array(stations(options, start))
+    longitudes = station_data[:, 0]
+    latitudes = station_data[:, 1]
+    temperatures = station_data[:, 2]
 
-# Expanded grid, larger than earth.
-xi = np.linspace(-360, 359, 720)
-yi = np.linspace(-180, 179, 360)
-zi = griddata((x_expanded, y_expanded),
-              temperatures_expanded,
-              (xi[None, :], yi[:, None]),
-              method='linear')
+    # make orthographic basemap.
+    m = Basemap(resolution='c', projection='cyl', lat_0=60., lon_0=-60.)
 
-CS1 = plt.contour(xi, yi, zi, 15, linewidths=0.5, colors='k', alpha=0.25)
-CS2 = plt.contourf(xi, yi, zi, 15, cmap=plt.cm.RdBu_r)
+    x, y = m(longitudes, latitudes)
+    x_expanded, y_expanded, temperatures_expanded = expand_earth(
+        x, y, temperatures)
 
-# Plot weather stations' locations as small blue dots.
-m.plot(x, y, 'b.', alpha=0.25)
+    # set desired contour levels.
+    clevs = np.arange(temperatures.min() - 10, temperatures.max() + 10, 5)
 
-# define parallels and meridians to draw.
-parallels = np.arange(-80., 90, 20.)
-meridians = np.arange(0., 360., 20.)
+    # create figure, add axes
+    fig1 = plt.figure(figsize=(8, 10))
+    ax = fig1.add_axes([0.1, 0.1, 0.8, 0.8])
 
-# draw coastlines, parallels, meridians.
-m.drawcoastlines(linewidth=1.5)
-m.drawparallels(parallels)
-m.drawmeridians(meridians)
-plt.show()
+    # Expanded grid, larger than earth.
+    xi = np.linspace(-360, 359, 720)
+    yi = np.linspace(-180, 179, 360)
+    zi = griddata((x_expanded, y_expanded),
+                  temperatures_expanded,
+                  (xi[None, :], yi[:, None]),
+                  method='linear')
+
+    CS1 = plt.contour(xi, yi, zi, 15, linewidths=0.5, colors='k', alpha=0.25)
+    CS2 = plt.contourf(xi, yi, zi, 15, cmap=plt.cm.RdBu_r)
+
+    # Plot weather stations' locations as small blue dots.
+    m.plot(x, y, 'b.', alpha=0.25)
+
+    # define parallels and meridians to draw.
+    parallels = np.arange(-80., 90, 20.)
+    meridians = np.arange(0., 360., 20.)
+
+    # draw coastlines, parallels, meridians.
+    m.drawcoastlines(linewidth=1.5)
+    m.drawparallels(parallels)
+    m.drawmeridians(meridians)
+    plt.show()
+
+
+if __name__ == '__main__':
+    main(sys.argv)
