9f531ea0657eb2a658e5e604005783e2a2945643
A. Jesse Jiryu Davis <jesse@mongodb.com>
2014-06-03 17:33:16 -0400

    Add geo-indexed coordinates to "flattened" collection.

--- viz/flatten-data.js
+++ viz/flatten-data.js
@@ -2,26 +2,38 @@
  * Convert ncdc.data collection into something flat that Monary can query.
  */
 
-db.data.aggregate([{
-    $match: {
-        ts: {$gt: ISODate("2013-12-01T00:00:00")},
-        // Valid temperature samples.
-        'airTemperature.quality': '1',
-        // Positions of 0, 0 are probably invalid.
-        'position.coordinates': {'$ne': [0, 0]}
+db.data.aggregate([
+    {
+        $match: {
+            ts: {$gt: ISODate("2013-12-01T00:00:00")},
+            // Valid temperature samples.
+            'airTemperature.quality': '1',
+            // Positions of 0, 0 are probably invalid.
+            'position.coordinates': {'$ne': [0, 0]}
+        }
+}, {
+    $project: {
+        ts: true,
+        position: true,
+        unwindablePosition: '$position',
+        airTemperature: true,
+        presentWeatherObservation: true
     }
 }, {
-    $unwind: "$position.coordinates"
+    $unwind: '$unwindablePosition.coordinates'
 }, {
     $group: {
         _id: '$_id',
         t: {$first: '$ts'},
-        x: {$first: '$position.coordinates'},
-        y: {$last: '$position.coordinates'},
-        a: {$first: '$airTemperature.value'}
+        x: {$first: '$unwindablePosition.coordinates'},
+        y: {$last: '$unwindablePosition.coordinates'},
+        c: {$first: '$position.coordinates'},
+        a: {$first: '$airTemperature.value'},
+        w: {$first: '$presentWeatherObservation.code'}
     }
 }, {
     $out: 'flattened'
 }], {allowDiskUse: true});
 
 db.flattened.ensureIndex({t: 1});
+db.flattened.ensureIndex({c: '2dsphere', w: 1});
